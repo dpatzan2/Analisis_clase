1e6e4ebc9d4af07d5465932a24d7e21f1f3ac1cf
Flaming Penguin <tsc221645@uvg.edu.gt>
2023-08-04 08:03:17 -0600

    menu

--- src/UI.java
+++ src/UI.java
@@ -1,8 +1,27 @@
-public class UI {
-    private static void Showmenu(){
+import java.util.Scanner;
 
+public class UI {
+
+    private static void Showmenu(){
+        String op = "";
+
+
+        System.out.println("----------------------------------");
+        System.out.println(" Programa De Palabras Palindromas ");
+        System.out.println("-----------------------------------");
+        System.out.println("");
+
+        Scanner in = new Scanner(System.in);
+        do{
+            System.out.println("Menu: ");
+            System.out.println("1. Leer archivo");
+            System.out.println("2. Salir");
+            System.out.println("Opcion: ");
+            op = in.nextLine();
+
+        }while(!op.equals("2"));
     }
     private static void ShowResults(){
-        
+        System.out.println("Mostrando");
     }
 }
