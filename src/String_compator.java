50c0742b26d1d215b1533d9a073ac453d48d1aff
Ihan-Marroquin <ihanmarroquin12@gmail.com>
2023-08-04 18:34:05 -0600

    Se agrego la lectura del csv y la busqueda de palabras palindromas

--- src/String_compator.java
+++ src/String_compator.java
@@ -1,34 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class String_compator {
-<<<<<<< HEAD
     public static void main(String[] args){
-        // Codigo sacado de Chat GPT
-        // Array de palabras
-        String[] palabras = {"Hola", "Mundo", "Java"};
+        String filePath = "./Libro1.csv"; // Cambia esta ruta por la ubicación real de tu archivo CSV
+        String[] palabras = CSV_Reader_Principal.readCSVFile(filePath);
 
-        // Crear un ArrayList para almacenar los caracteres
-        ArrayList<Character> caracteres = new ArrayList<>();
+        // Generar un nuevo array con las palabras palíndromas
+        String[] palindromas = obtenerPalindromas(palabras);
 
-        // Recorrer el array de palabras
+        // Mostrar las palabras palíndromas en el nuevo array
+        System.out.println("Palíndromas:");
+        for (String palabra : palindromas) {
+            System.out.println(palabra);
+        }
+    }
+
+    public static String[] obtenerPalindromas(String[] palabras) {
+        List<String> palindromasList = new ArrayList<>();
+
         for (String palabra : palabras) {
-            // Recorrer cada palabra y agregar los caracteres al ArrayList
-            for (char caracter : palabra.toCharArray()) {
-                caracteres.add(caracter);
+            if (esPalindromo(palabra)) {
+                palindromasList.add(palabra);
             }
         }
 
-        // Convertir el ArrayList en un array de caracteres
-        char[] arrayCaracteres = new char[caracteres.size()];
-        for (int i = 0; i < caracteres.size(); i++) {
-            arrayCaracteres[i] = caracteres.get(i);
-        }
-
-        // Imprimir el array de caracteres
-        for (char c : arrayCaracteres) {
-            System.out.print(c + " ");
-        }
-
+        return palindromasList.toArray(new String[0]);
     }
-=======
 
->>>>>>> b0ff238142bb5ac949485d4b4243edfd2c05dacf
+    public static boolean esPalindromo(String palabra) {
+        String reversed = new StringBuilder(palabra).reverse().toString();
+        return palabra.equalsIgnoreCase(reversed);
+    }
 }
